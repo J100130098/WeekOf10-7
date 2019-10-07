10445ef6aac475c6664c9b0c1f0f21f5067f4407
100130098 <jjhill080103@gmail.com>
2019-10-07 08:44:08 -0500

    Bruh

--- WeekOf10-7.py
+++ WeekOf10-7.py
@@ -1,10 +1,14 @@
+# The Four lines of readable below are defining what x, y, binary, and doNot mean and what they will display when they are printed.
 x = "there are %d types of people." % 10
 binary = "binary"
 doNot = "don't"
+# The line below uses %s to determine what will be printed when it is. The first %s will display text associated with binary, while the second %s will display text associated with doNot.
 y = "Those  who %s and those who %s" % (binary, doNot)
 
+# The two lines below will print the text associated with x and y
 print(x)
 print(y)
+
 
 print("I said: %r" % x)
 print("I also said: '%s'." % y)
