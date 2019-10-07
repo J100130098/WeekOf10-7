1256e4e232cfedb46cf57665c632351767ddd178
100130098 <jjhill080103@gmail.com>
2019-10-07 08:52:42 -0500

    Bruh

--- WeekOf10-7.py
+++ WeekOf10-7.py
@@ -5,24 +5,25 @@ doNot = "don't"
 # The line below uses %s to determine what will be printed when it is. The first %s will display text associated with binary, while the second %s will display text associated with doNot.
 y = "Those  who %s and those who %s" % (binary, doNot)
 
-# The two lines below will print the text associated with x and y
+# The two lines below will print the text associated with x and y.
 print(x)
 print(y)
 
-# The two lines below add text before both x and y
+# The two lines below add text before both x and y.
 print("I said: %r" % x)
 print("I also said: '%s'." % y)
 
-# The line below is defining the term hilarious, which is false
+# The line below is defining the term hilarious, which is false.
 hilarious = False
-# the line below is defining what the term joke_evaluation will print when it is printed
+# the line below is defining what the term joke_evaluation will print when it is printed.
 joke_evaluation = "Isn't that joke so funny!? %r"
-# the line below prints the defined terms from above, stating that the joke was not funny
+# the line below prints the defined terms from above, stating that the joke was not funny.
 print(joke_evaluation % hilarious)
 
-
+# the two lines below define what w and e will be printed as when given the command print.
 w = "This is the left side of..."
 e = "A string with a right side."
 
+# the line below is printing the text associated with w and e together, making one complete sentence.
 print(w + e)
 
