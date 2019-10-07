7e1199b1ffab9a1696aff13e1ece68003843eea8
100130098 <jjhill080103@gmail.com>
2019-10-07 08:36:37 -0500

    Bruh

---
+++ WeekOf10-7.py
@@ -0,0 +1,21 @@
+x = "there are %d types of people." % 10
+binary = "binary"
+doNot = "don't"
+y = "Those  who %s and those who %s" % (binary, doNot)
+
+print(x)
+print(y)
+
+print("I said: %r" % x)
+print("I also said: '%s'." % y)
+
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny!? %r"
+print(joke_evaluation % hilarious)
+
+w = "This is the left side of..."
+e = "A string with a right side."
+
+print(w + e)
+
