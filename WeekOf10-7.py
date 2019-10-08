c27bb2eed35fc7cdbb5e1a4bc661d24da562090d
100130098 <jjhill080103@gmail.com>
2019-10-08 08:32:08 -0500

    Commit 10-8

--- WeekOf10-7.py
+++ WeekOf10-7.py
@@ -27,3 +27,24 @@ e = "A string with a right side."
 # the line below is printing the text associated with w and e together, making one complete sentence.
 print(w + e)
 
+# 10-8 more stuff
+
+print("Mary had a little lamb.")
+print("it's fleece was white as %s." % 'snow')
+print("And everywhere that Mary went.")
+print("." * 10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11 + end12)
